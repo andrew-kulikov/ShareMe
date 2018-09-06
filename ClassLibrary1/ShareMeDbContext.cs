145f15da8e44a46795971df2a7c84d23d8fc96e6
Andrew <andrew.1.kulikov@gmail.com>
2018-09-06 21:47:13 +0300

    Add data access layer

---
+++ ClassLibrary1/ShareMeDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShareMe.Core
+{
+	public class ShareMeDbContext : DbContext
+	{
+		public ShareMeDbContext(DbContextOptions<ShareMeDbContext> options) : base(options) { }
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+			// Customize the ASP.NET Identity model and override the defaults if needed.
+			// For example, you can rename the ASP.NET Identity table names and more.
+			// Add your customizations after calling base.OnModelCreating(builder);
+		}
+	}
+}
