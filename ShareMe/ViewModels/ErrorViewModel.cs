033677fa53a490b5ff3c4d7d7263a42855653849
Andrew <andrew.1.kulikov@gmail.com>
2018-09-07 21:14:08 +0300

    Rename Models namespace to ViewModels

---
+++ ShareMe/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShareMe.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ShareMe/ViewModels/PhotoViewModels/PhotoFormViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShareMe.ViewModels.PhotoViewModels
+{
+    public class PhotoFormViewModel
+    {
+		[Required]
+	    public string Url { get; set; }
+
+		[StringLength(1000)]
+	    public string Description { get; set; }
+    }
+}
