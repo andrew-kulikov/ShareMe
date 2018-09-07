e0227885946af926d9607c95c4412a48504dd4da
Andrew <andrew.1.kulikov@gmail.com>
2018-09-07 21:15:00 +0300

    Move api controllers to separate folder

---
+++ ShareMe/Controllers/api/LoginController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using ShareMe.Core.Models;
+using ShareMe.ViewModels.AccountViewModels;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShareMe.Controllers.api
+{
+	[Produces("application/json")]
+	[Route("api/Login")]
+	public class LoginController : Controller
+	{
+		private readonly UserManager<AspNetUsers> _userManager;
+		private readonly SignInManager<AspNetUsers> _signInManager;
+		private readonly IConfiguration _configuration;
+
+
+		public LoginController(UserManager<AspNetUsers> userManager, SignInManager<AspNetUsers> signInManager,
+			IConfiguration configuration)
+		{
+			_userManager = userManager;
+			_signInManager = signInManager;
+			_configuration = configuration;
+		}
+
+		[AllowAnonymous]
+		[HttpPost]
+		[Route("GenerateToken")]
+		public async Task<IActionResult> GenerateToken([FromBody] LoginViewModel model)
+		{
+			if (!ModelState.IsValid) return BadRequest("Could not create token");
+			var user = await _userManager.FindByEmailAsync(model.Email);
+
+			if (user == null) return BadRequest("Could not create token");
+			var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
+			if (!result.Succeeded) return BadRequest("Could not create token");
+			var claims = new[]
+			{
+				new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+				new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+				new Claim(JwtRegisteredClaimNames.Email, user.Email),
+				new Claim("Foo", "Bar")
+			};
+
+			var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Tokens:Key"]));
+			var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+			var token = new JwtSecurityToken(_configuration["Tokens:Issuer"],
+				_configuration["Tokens:Issuer"],
+				claims,
+				expires: DateTime.Now.AddMinutes(30),
+				signingCredentials: creds);
+
+			return Ok(new {token = new JwtSecurityTokenHandler().WriteToken(token)});
+		}
+
+		[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+		[HttpGet]
+		public IEnumerable<string> Get()
+		{
+			return new[] {"value1", "value2"};
+		}
+	}
+}
---
+++ ShareMe/Controllers/api/PhotoController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShareMe.Core;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using ShareMe.Core.Models;
+
+namespace ShareMe.Controllers.api
+{
+	[Produces("application/json")]
+	[Route("/api/photos")]
+	public class PhotoController : Controller
+	{
+		private ShareMeDbContext _context;
+		private readonly UserManager<AspNetUsers> _userManager;
+
+		public PhotoController(UserManager<AspNetUsers> userManager)
+		{
+			_context = new ShareMeDbContext(new DbContextOptions<ShareMeDbContext>());
+			_userManager = userManager;
+
+		}
+
+		[HttpPost("add")]
+		public async Task<ActionResult> AddPhoto([FromBody]string url)
+		{
+			var userName = User.Identity.Name;
+
+			var user = await _userManager.GetUserAsync(HttpContext.User);
+
+			return Ok(user);
+		}
+
+		[Authorize]
+		[HttpGet]
+		public async Task<ActionResult> UserCheck()
+		{
+			var userName = User.Identity.Name;
+			var user = await _userManager.GetUserAsync(HttpContext.User);
+
+			return Ok(user);
+		}
+
+	}
+}
