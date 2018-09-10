ee327469ecfae92adcc71931bd12b269b5ed0bf7
Andrew <andrew.1.kulikov@gmail.com>
2018-09-10 23:32:03 +0300

    Populate RatingTypeTable

---
+++ ClassLibrary1/Migrations/20180910202609_PopulateRatingTypesTable.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShareMe.Core.Migrations
+{
+	public partial class PopulateRatingTypesTable : Migration
+	{
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.Sql("Insert into RatingType Values ('Like'), ('Dislike')");
+		}
+
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.Sql("remove from RatingType where Name = 'Like' or Name ='Dislike'");
+		}
+	}
+}
