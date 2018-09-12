c3557385b05ea7316533edb463c408a219ad9b6c
Andrew <andrew.1.kulikov@gmail.com>
2018-09-12 15:09:23 +0300

    Move following js logic to separate files

---
+++ ShareMe/wwwroot/js/Controllers/PhotoDetailsController.js
@@ -0,0 +1,32 @@
+﻿var PhotoDetailsController = function (followingService) {
+    var button;
+
+    var done = function () {
+        var text = button.text() === "Following" ? "Follow" : "Following";
+
+        button.toggleClass("btn-default").toggleClass("btn-info").text(text);
+    };
+
+    var fail = function () {
+        alert("Something failed!");
+    };
+
+    var toggleFollowing = function (e) {
+        button = $(e.target);
+
+        var userId = button.attr("data-user-id");
+
+        if (button.hasClass("btn-info"))
+            followingService.follow(userId, done, fail);
+        else
+            followingService.unfollow(userId, done, fail);
+    };
+
+    var init = function () {
+        $(".js-toggle-following").click(toggleFollowing);
+    };
+
+    return {
+        init: init
+    };
+}(FollowingsService);
---
+++ ShareMe/wwwroot/js/Services/FollowingsService.js
@@ -0,0 +1,21 @@
+﻿var FollowingsService = function () {
+    var follow = function (followeeId, done, fail) {
+        $.post("/api/followings/add/" + followeeId)
+            .done(done)
+            .fail(fail);
+    };
+
+    var unfollow = function (followeeId, done, fail) {
+        $.ajax({
+                url: "/api/followings/delete/" + followeeId,
+                method: "DELETE"
+            })
+            .done(done)
+            .fail(fail);
+    };
+
+    return {
+        follow: follow,
+        unfollow: unfollow
+    };
+}();
