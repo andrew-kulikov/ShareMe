478a21485155c79fa9f3d107e7cc2b77a29f4a26
Andrew <andrew.1.kulikov@gmail.com>
2018-09-14 15:07:39 +0300

    Add patial comments view

---
+++ ShareMe/ViewModels/CommentsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ShareMe.Core.Models;
+
+namespace ShareMe.ViewModels
+{
+    public class CommentsViewModel
+    {
+	    public ICollection<Comment> Comments { get; set; }
+    }
+}
