4ff16717b61ed22de043724332d9c791b39ef3f0
Andrew <andrew.1.kulikov@gmail.com>
2018-09-15 14:39:41 +0300

    Move like logic to separate js file

--- ShareMe/wwwroot/js/Controllers/PhotoDetailsController.js
+++ ShareMe/wwwroot/js/Controllers/PhotoDetailsController.js
@@ -1,11 +1,27 @@
-﻿var PhotoDetailsController = function (followingService) {
+﻿var PhotoDetailsController = function (followingService, ratingService) {
     var button;
+    var likeImg;
+    var photoId;
 
-    var done = function () {
+    var doneFollowing = function () {
         var text = button.text().trim() === "Following" ? "Follow" : "Following";
 
         button.toggleClass("btn-default").toggleClass("btn-info").text(text);
     };
+
+    var doneLike = function() {
+        var count = parseInt($("#likes").html());
+        $("#likes").html(count + 1);
+        $(likeImg).attr("liked", "1");
+        $(likeImg).css("color", "red");
+    };
+
+    var doneDislike = function() {
+        var count = parseInt($("#likes").html());
+        $("#likes").html(count - 1);
+        $(likeImg).removeAttr("liked");
+        $(likeImg).css("color", "black");
+    }
 
     var fail = function () {
         alert("Something failed!");
@@ -17,16 +33,27 @@
         var userId = button.attr("data-user-id");
 
         if (button.hasClass("btn-info"))
-            followingService.follow(userId, done, fail);
+            followingService.follow(userId, doneFollowing, fail);
         else
-            followingService.unfollow(userId, done, fail);
+            followingService.unfollow(userId, doneFollowing, fail);
+    };
+
+    var toggleLike = function (e) {
+        photoId = $("#data").attr("data-photo-id");
+        likeImg = $("#like-img");
+        if ($(likeImg)[0].hasAttribute("liked")) {
+            ratingService.dislike(photoId, doneDislike, fail);
+        } else {
+            ratingService.like(photoId, doneLike, fail);
+        }
     };
 
     var init = function () {
         $(".js-toggle-following").click(toggleFollowing);
+        $("#like").click(toggleLike);
     };
 
     return {
         init: init
     };
-}(FollowingsService);
+}(FollowingsService, RatingsService);
---
+++ ShareMe/wwwroot/js/Services/RatingsService.js
@@ -0,0 +1,21 @@
+﻿var RatingsService = function () {
+    var like = function (photoId, done, fail) {
+        $.post("/api/photos/like/" + photoId)
+            .done(done)
+            .fail(fail);
+    };
+
+    var dislike = function (photoId, done, fail) {
+        $.ajax({
+                url: "/api/photos/dislike/" + photoId,
+                method: "DELETE"
+            })
+            .done(done)
+            .fail(fail);
+    };
+
+    return {
+        like: like,
+        dislike: dislike
+    };
+}();
